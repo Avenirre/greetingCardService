1a2f075ac09e1375f8b659cbf6bc2d29b12b46ab
Роман Димитриевич <romandimitrievich@MacBook-Air-User.local>
2019-01-24 23:23:59 +0200

    initial commit

---
+++ greetingCards/src/main/java/com/gc/GreetingCardsApplication.java
@@ -0,0 +1,14 @@
+package com.gc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingCardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreetingCardsApplication.class, args);
+	}
+
+}
+
