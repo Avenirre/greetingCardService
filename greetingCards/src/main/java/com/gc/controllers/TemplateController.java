befeacd60ffe58d4e834384945e50afbf4170682
romandimitrievich <romandimitrievich@192.168.0.100>
2019-01-28 02:50:03 +0200

    adding 'error' page

--- greetingCards/src/main/java/com/gc/controllers/TemplateController.java
+++ greetingCards/src/main/java/com/gc/controllers/TemplateController.java
@@ -1,15 +1,22 @@
 package com.gc.controllers;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.gc.exceptions.NotFoundException;
 import com.gc.model.TemplateDto;
 import com.gc.services.TemplateService;
+
+import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @RequestMapping("greetingcards/")
@@ -37,6 +44,14 @@ public class TemplateController {
     public String saveTemplate(@ModelAttribute("template") TemplateDto template, @PathVariable String id){
         templateService.updateAndSaveTemplate(template);
         return "redirect:/greetingcards/catalog";
+    }
+    
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public ModelAndView handleNotFound(Exception exception){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("error");
+        return modelAndView;
     }
 
 }
--- greetingCards/src/main/java/com/gc/exceptions/NotFoundException.java
+++ greetingCards/src/main/java/com/gc/exceptions/NotFoundException.java
@@ -11,7 +11,15 @@ public class NotFoundException extends RuntimeException {
 	 */
 	private static final long serialVersionUID = 1L;
 
-	public NotFoundException(String message) {
+    public NotFoundException() {
+        super();
+    }
+
+    public NotFoundException(String message) {
         super(message);
+    }
+
+    public NotFoundException(String message, Throwable cause) {
+        super(message, cause);
     }
 }
